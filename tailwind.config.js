f3e8e4ec6f3a92feb73ed79cc2bbbe5ac7f523d2
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-07-26 12:30:21 +0530

    add new bounce animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
       fontFamily: {
         noto: "'Noto Sans', 'sans-serif'",
       },
+      keyframes: {
+        upDown: {
+          "0% , 50% , 100%": { transform: "translateY(0) rotate(45deg)" },
+          "25%": { transform: "translateY(-20px) rotate(45deg)" },
+          "75%": { transform: "translateY(20px) rotate(45deg)" },
+        },
+      },
+      animation: {
+        upDown: "upDown  5s linear infinite",
+      },
     },
   },
   plugins: [require("flowbite/plugin")],
