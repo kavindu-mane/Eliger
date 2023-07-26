bbadc5ff0017b602ec535f63b2bb0e023ad4534c
Yassin-Aru <jocarhisouka@gmail.com>
2023-07-26 11:30:44 +0100

    create data folder

---
+++ src/data/faqData.js
@@ -0,0 +1,40 @@
+const faqData = [
+    {
+        id: 1,
+        question: "What is your return policy?",
+        answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis illum doloremque magni ex corrupti tempora quis.",
+    },
+    {
+        id: 2,
+        question: "Do you offer international shipping?",
+        answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis illum doloremque magni ex corrupti tempora quis.",
+    },
+    {
+        id: 3,
+        question: "How can I track my order?",
+        answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis illum doloremque magni ex corrupti tempora quis.",
+    },
+    {
+        id: 4,
+        question: "What payment methods do you accept?",
+        answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis illum doloremque magni ex corrupti tempora quis.",
+    },
+    {
+        id: 5,
+        question: "Do you offer gift wrapping?",
+        answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis illum doloremque magni ex corrupti tempora quis.",
+    },
+    {
+        id: 6,
+        question: "Can I cancel my order after it's been placed?",
+        answer:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident placeat, consequatur eveniet veritatis quos dignissimos beatae dolores exercitationem laboriosam officia magnam atque blanditiis illum doloremque magni ex corrupti tempora quis.",
+    }
+];
+
+export default faqData;
