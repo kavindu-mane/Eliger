29ddf8be2961fe3ec97a5beb46bf275c93737f1b
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-07-26 12:32:00 +0530

    remove unwanted jsx div element

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init({
+  duration: 800,
+  easing: "ease-in",
+  once: true,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="bg-white text-black dark:bg-slate-900 dark:text-white">
-      <App />
-    </div>
+    <App />
   </React.StrictMode>
 );
 
