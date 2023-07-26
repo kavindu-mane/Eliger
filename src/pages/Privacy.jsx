35fa95bd7a1efbbc8b95c0181e804cf832343b5c
pabasaraweliwaththa71 <cst20071@std.uwu.ac.lk>
2023-07-26 17:49:13 +0700

    edit privacy page

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -1,13 +1,14 @@
 import React, { lazy } from "react";
 const Header = lazy(() => import("../components/Header"));
 const Footer = lazy(() => import("../components/Footer"));
+import PrivacyAccordion from "../components/privacy/PrivacyAccordion.jxs";
 
 const Privacy = () => {
   return (
     <React.Fragment>
       <div className="flex min-h-screen flex-col items-center justify-between">
         <Header />
-
+<PrivacyAccordion/>
         <Footer />
       </div>
     </React.Fragment>
