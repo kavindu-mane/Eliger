a8404c338f3391dd59ee8d4badae34278ae51da9
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-07-27 17:29:46 +0530

    remove title area and use new titles component

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -6,31 +6,30 @@ import PrivacyData from "../components/Data/PrivacyData";
 const Accordions = lazy(() => import("../components/Accordions"));
 const Header = lazy(() => import("../components/Header"));
 const Footer = lazy(() => import("../components/Footer"));
+const Titles = lazy(() => import("../components/Titles"));
 
 const Privacy = () => {
   return (
     <React.Fragment>
       <Header />
-      {/* title section */}
-      <div className="my-20 w-screen text-center font-Poppins">
-        <h3 className="text-center text-4xl font-bold text-green-400 md:text-5xl">
-          Privacy Policy
-        </h3>
 
-        <h5 className="text-md mt-3 text-center text-green-500 dark:text-green-400">
+      {/* title section */}
+      <Titles
+        title={"Privacy Policy"}
+        subtitle={"collects some personal data from its users."}
+      >
+        <span className=" me-1.5 text-green-500 dark:text-green-400">
           Eliger
-          <span className="ms-1.5 text-slate-800 dark:text-white">
-            collects some personal data from its users.
-          </span>
-        </h5>
-      </div>
+        </span>
+      </Titles>
+
       {/* content section */}
       <div className="mx-3 my-20 grid grid-cols-1 gap-x-4 md:mx-10 md:grid-cols-2 md:gap-x-16 xl:gap-x-32">
         {/* left side */}
         <div className="flex h-full w-full flex-col items-center justify-center md:items-end">
           <div className="max-w-lg">
             <p
-              className="mb-3 text-justify font-ABeeZee text-slate-900 dark:text-white"
+              className="mb-3 text-justify font-ABeeZee text-slate-900 dark:text-slate-100"
               data-aos="fade-right"
             >
               &emsp;&emsp;Welcome to our Eliger Vehicle Renting System! We are
