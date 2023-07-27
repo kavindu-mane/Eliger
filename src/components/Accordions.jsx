e78098c9859c3ab7c891172a0d8d501961c565f0
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-07-28 00:26:11 +0530

    class changes

--- src/components/Accordions.jsx
+++ src/components/Accordions.jsx
@@ -13,11 +13,11 @@ const Accordions = ({ datafile }) => {
         {datafile.map((data) => {
           return (
             <Accordion.Panel key={data.id}>
-              <Accordion.Title className="relative mb-3 rounded-t-xl border-none bg-white font-ABeeZee ring-1 ring-slate-300 focus:ring-1 focus:ring-slate-300 dark:bg-transparent dark:ring-slate-700 dark:focus:ring-slate-700">
+              <Accordion.Title className="relative mb-3 rounded-t-xl border-none bg-white font-ABeeZee ring-1 ring-slate-300 focus:ring-1 focus:ring-slate-300 dark:bg-transparent dark:ring-slate-600 dark:focus:ring-slate-600 text-slate-600 dark:text-slate-400">
                 <span className="me-5">{String(data.id).padStart(2, "0")}</span>
                 {data.question}
               </Accordion.Title>
-              <Accordion.Content className="-mt-3 mb-5 rounded-b-xl bg-sky-50 ring-1 ring-slate-300 dark:ring-slate-700">
+              <Accordion.Content className="-mt-3 mb-5 rounded-b-xl bg-sky-50 ring-1 ring-slate-300 dark:ring-slate-600">
                 <p className="mb-2 font-ABeeZee text-gray-600 dark:text-gray-400">
                   {data.answer}
                 </p>
