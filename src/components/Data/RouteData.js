3e0ec5c6f3617c04df7a09e29848c72e9fddfa2c
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-07-28 22:42:09 +0530

    new data files added

---
+++ src/components/Data/RouteData.js
@@ -0,0 +1,24 @@
+import React, { lazy } from "react";
+const Home = lazy(() => import("../../pages/Home"));
+const About = lazy(() => import("../../pages/About"));
+const Contact = lazy(() => import("../../pages/Contact"));
+const Rent = lazy(() => import("../../pages/Rent"));
+const Faq = lazy(() => import("../../pages/Faq"));
+const Privacy = lazy(() => import("../../pages/Privacy"));
+const Ride = lazy(() => import("../../pages/Ride"));
+const Terms = lazy(() => import("../../pages/Terms"));
+const Error = lazy(() => import("../../pages/Error"));
+
+const LinkArray = {
+  "/": <Home />,
+  "/about": <About />,
+  "/contact": <Contact />,
+  "/rent": <Rent />,
+  "/ride": <Ride />,
+  "/faq": <Faq />,
+  "/privacy": <Privacy />,
+  "/terms": <Terms />,
+  "/*": <Error />,
+};
+
+export default LinkArray;
---
+++ src/components/Data/ThmeSwitecher.js
@@ -0,0 +1,13 @@
+const ThemeSwitcher = () => {
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+};
+
+export default ThemeSwitcher;
