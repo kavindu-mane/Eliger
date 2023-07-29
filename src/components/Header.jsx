d5cb257ad1a247b35f36d901cd3b4c2a8fdc5388
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-07-29 20:46:22 +0530

    add login button with change classes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-import React, { useState, lazy } from "react";
+import React, { useState } from "react";
 import Logo from "../resources/Eliger-white.png";
 import { RiCloseLine, RiMenu3Fill } from "react-icons/ri";
-const HeaderLinks = lazy(() => import("./HeaderLinks"));
+import HeaderData from "./Data/HeaderData";
 
 const Header = ({ bg = "bg-slate-800" }) => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -33,7 +33,7 @@ const Header = ({ bg = "bg-slate-800" }) => {
         {/* header link area */}
         <div
           id="menu"
-          className="fixed -right-80 top-0 z-[999] flex h-screen flex-col bg-slate-700 pt-5 duration-300 md:relative md:right-0 md:z-0 md:me-4 md:h-auto md:flex-row md:bg-transparent md:p-0"
+          className="fixed -right-80 top-0 z-[999] flex h-screen flex-col bg-slate-700 pt-5 duration-300 md:relative md:right-0 md:z-0 md:me-8 md:h-auto md:flex-row md:bg-transparent md:p-0"
         >
           {/* menu button */}
           <button
@@ -42,12 +42,18 @@ const Header = ({ bg = "bg-slate-800" }) => {
           >
             <RiCloseLine className="h-6 w-6 text-white md:hidden" />
           </button>
-          <HeaderLinks link={"/rent"} text={"Rent"} />
-          <HeaderLinks link={"/ride"} text={"Ride"} />
-          <HeaderLinks link={"/privacy"} text={"Privacy"} />
-          <HeaderLinks link={"/about"} text={"About"} />
-          <HeaderLinks link={"/contact"} text={"Contact"} />
-          <HeaderLinks link={"/faq"} text={"FAQ"} />
+          {HeaderData.map((data) => {
+            return (
+              <a
+                href={data.link}
+                className={
+                  "h-fit w-80 place-self-center rounded-sm px-5 py-3 font-Poppins font-semibold text-white duration-200 ease-in last:mt-5 last:w-fit last:bg-green-400 last:px-8 last:py-1.5 last:text-center hover:bg-slate-800 hover:text-cyan-400 last:hover:bg-orange-400 last:hover:text-white md:w-auto md:py-1.5 md:last:ms-5 md:last:mt-0 md:last:px-5 md:hover:bg-transparent last:shadow-md last:drop-shadow-md"
+                }
+              >
+                {data.text}
+              </a>
+            );
+          })}
         </div>
       </div>
     </React.Fragment>
