6e5e63c9bf61f1343010587d5ff1624bf7a9627f
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-07-30 00:12:41 +0530

    improve header and UX

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,54 +6,90 @@ import HeaderData from "./Data/HeaderData";
 const Header = ({ bg = "bg-slate-800" }) => {
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const menuSettings = (e) => {
+  const menuSettings = () => {
     const MENU = document.getElementById("menu").classList;
+    const documentElement = document.documentElement.classList;
     if (menuOpen) {
-      MENU.replace("right-0", "-right-80");
+      MENU.replace("right-0", "-right-full");
+      documentElement.remove("overflow-hidden");
       setMenuOpen(false);
     } else {
-      MENU.replace("-right-80", "right-0");
+      MENU.replace("-right-full", "right-0");
+      documentElement.add("overflow-hidden");
       setMenuOpen(true);
+    }
+  };
+
+  // set menu open false with screen resize
+  window.onresize = () => {
+    if (window.innerWidth > 768 && menuOpen) {
+      menuSettings();
     }
   };
 
   return (
     <React.Fragment>
-      <div className={"flex w-screen items-center justify-between py-2 " + bg}>
+      <div
+        className={
+          "flex w-screen items-center justify-between py-2 font-Poppins font-semibold " +
+          bg
+        }
+      >
         {/* logo */}
         <a href="/">
           <img src={Logo} alt="logo" className="z-50 ms-4 w-32 md:w-36" />
         </a>
 
         {/* menu button */}
-        <button onClick={(e) => menuSettings(e)} className="me-4">
-          <RiMenu3Fill className="h-6 w-6 text-white md:hidden" />
+        <button onClick={menuSettings} className="z-[999] me-4 md:hidden">
+          {!menuOpen ? (
+            <RiMenu3Fill className="h-6 w-6 text-white duration-200 hover:text-cyan-400" />
+          ) : (
+            <RiCloseLine className="h-6 w-6 text-white duration-200 hover:text-cyan-400" />
+          )}
         </button>
 
-        {/* header link area */}
-        <div
-          id="menu"
-          className="fixed -right-80 top-0 z-[999] flex h-screen flex-col bg-slate-700 pt-5 duration-300 md:relative md:right-0 md:z-0 md:me-8 md:h-auto md:flex-row md:bg-transparent md:p-0"
-        >
-          {/* menu button */}
-          <button
-            onClick={(e) => menuSettings(e)}
-            className="mb-5 me-4 w-fit place-self-end"
-          >
-            <RiCloseLine className="h-6 w-6 text-white md:hidden" />
-          </button>
+        {/* header link area horizontal*/}
+        <div className="me-8 hidden md:flex">
           {HeaderData.map((data) => {
             return (
               <a
+                key={data.text}
                 href={data.link}
                 className={
-                  "h-fit w-80 place-self-center rounded-sm px-5 py-3 font-Poppins font-semibold text-white duration-200 ease-in last:mt-5 last:w-fit last:bg-green-400 last:px-8 last:py-1.5 last:text-center hover:bg-slate-800 hover:text-cyan-400 last:hover:bg-orange-400 last:hover:text-white md:w-auto md:py-1.5 md:last:ms-5 md:last:mt-0 md:last:px-5 md:hover:bg-transparent last:shadow-md last:drop-shadow-md"
+                  "h-fit w-auto rounded-sm px-5 py-1.5 text-white duration-200 ease-in last:ms-5 last:bg-green-400 last:px-5 last:py-1.5 last:shadow-md last:drop-shadow-md hover:text-cyan-400 last:hover:bg-orange-400 last:hover:text-white md:hover:bg-transparent"
                 }
               >
                 {data.text}
               </a>
             );
           })}
+        </div>
+
+        {/* header link area vertical */}
+        <div
+          id="menu"
+          className="fixed -right-full top-0 z-[99] flex h-screen w-screen duration-200"
+        >
+          <div
+            className="h-full w-full backdrop-blur-sm"
+            onClick={menuSettings}
+          ></div>
+          <div className="flex flex-col bg-slate-700 pt-16">
+            {HeaderData.map((data) => {
+              return (
+                <a
+                  key={data.text}
+                  href={data.link}
+                  className={
+                    "h-fit w-80 place-self-center rounded-sm px-5 py-3 text-white duration-200 ease-in last:mt-5 last:w-fit last:bg-green-400 last:px-8 last:py-1.5 last:text-center last:shadow-md last:drop-shadow-md hover:bg-slate-800 hover:text-cyan-400 last:hover:bg-orange-400 last:hover:text-white"
+                  }
+                >
+                  {data.text}
+                </a>
+              );
+            })}
+          </div>
         </div>
       </div>
     </React.Fragment>
