21b8e1c384c6f2a875d1027e884ac8ed302dc9a2
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-07-29 20:45:47 +0530

    create header link data file

---
+++ src/components/Data/HeaderData.js
@@ -0,0 +1,28 @@
+const HeaderData = [
+  {
+    link: "/rent",
+    text: "Rent",
+  },
+  {
+    link: "/ride",
+    text: "Ride",
+  },
+  {
+    link: "/about",
+    text: "About",
+  },
+  {
+    link: "/contact",
+    text: "Contact",
+  },
+  {
+    link: "/faq",
+    text: "FAG",
+  },
+  {
+    link: "/login",
+    text: "Login",
+  },
+];
+
+export default HeaderData;
