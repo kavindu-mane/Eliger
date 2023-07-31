b56f4ed334285556db65448139e79ae5e69350a5
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-08-01 01:30:54 +0530

    add new theme changer button

--- src/components/HeaderSecondary.jsx
+++ src/components/HeaderSecondary.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, { lazy } from "react";
 import images from "./Data/ImageLoader";
+const ThemeButton = lazy(() => import("./ThemeButton"));
 
 const HeaderSecondary = () => {
   return (
-    <div className="absolute start-0 top-0 my-2 flex h-fit w-fit">
+    <div className="absolute start-0 top-0 my-2 flex h-fit w-screen items-center justify-between">
       {/* dark theme logo */}
-      <a href="/">
-        <img
-          src={images["Eliger-white.png"]}
-          alt="logo dark"
-          className="ms-4 hidden w-32 dark:block md:w-36"
-        />
-      </a>
-      {/* light theme logo */}
-      <a href="/">
-        <img
-          src={images["Eliger.png"]}
-          alt="logo light"
-          className="ms-4 block w-32 dark:hidden md:w-36"
-        />
-      </a>
+      <div className="h-fit-w-fit flex">
+        <a href="/">
+          <img
+            src={images["Eliger-white.png"]}
+            alt="logo dark"
+            className="ms-4 hidden w-32 dark:block md:w-36"
+          />
+        </a>
+        {/* light theme logo */}
+        <a href="/">
+          <img
+            src={images["Eliger.png"]}
+            alt="logo light"
+            className="ms-4 block w-32 dark:hidden md:w-36"
+          />
+        </a>
+      </div>
+      {/* theme button */}
+      <ThemeButton placement="bottom" />
     </div>
   );
-}
+};
 
-export default HeaderSecondary
+export default HeaderSecondary;
