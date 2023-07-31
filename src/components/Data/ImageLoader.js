5720a7744bee757c79d3eaa024edc41b17382c53
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-07-31 16:43:32 +0530

    create all image loader file

---
+++ src/components/Data/ImageLoader.js
@@ -0,0 +1,13 @@
+const importAll = (r) => {
+  let images = {};
+  r.keys().forEach((item, index) => {
+    images[item.replace("./", "")] = r(item);
+  });
+  return images;
+};
+
+const images = importAll(
+  require.context("../../resources/", false, /.(png|jpe?g|svg)$/)
+);
+
+export default images;
