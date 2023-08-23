7f99ed01acc116c21504985d41fddd08466c0b69
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-08-24 02:12:47 +0530

    change class names

--- src/components/common/BackgroundEffect.jsx
+++ src/components/common/BackgroundEffect.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BackgroundEffect = () => {
   return (
-    <div className="absolute start-0 top-1/2 -z-50 h-1/3 w-screen -translate-y-1/2 bg-gradient-to-r from-emerald-400/50 to-sky-400/50 blur-[100px] dark:from-emerald-400/[0.23] dark:to-sky-500/[0.23]"></div>
+    <div className="absolute start-0 top-1/2 -z-50 h-1/3 w-screen -translate-y-1/2 bg-gradient-to-r from-emerald-400/40 to-sky-400/30 blur-[100px] dark:from-emerald-400/[0.23] dark:to-sky-500/[0.23]"></div>
   );
 };
 
