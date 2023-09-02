0c2aa7c3997fdede426958f69fc61ea735ffc988
Upeksha Dhananaji <upekshadhananji123@gmail.com>
2023-09-02 19:51:13 +0530

    Customer Dashboard

--- src/Components/Customer/EditMyProfile.jsx
+++ src/Components/Customer/EditMyProfile.jsx
@@ -28,7 +28,7 @@ const EditMyProfile = () => {
   const [errorCode, setErrorCode] = useState(null);
   const [ setIsLoading] = useState(false);
   const [setIsSuccess] = useState(false);
-  const [setEmail] = useState("");
+ 
 
   // custom allert function with sweet alert 2
   const setAlert = (icon, title, desc) => {
@@ -69,179 +69,171 @@ const EditMyProfile = () => {
   };
 
   return (
-    <Card className="w-full max-w-2xl">
+    <Card className="w-full max-w-2xl border-none bg-transparent dark:bg-transparent">
       {/*Topic */}
       <h1 className="text-1.5xl font-bold ">Edit Account Details</h1>
       <form className="flex flex-col gap-4">
-        <div>
-           className="flex w-full max-w-lg flex-col gap-4"
-              onSubmit={(e) => handleSubmit(e)}
-          <div className="mb-2 block">
-            {/* name */}
-            <div className="flex flex-col justify-between sm:flex-row">
-              {/* first name */}
-              <div className="mb-4 w-full sm:mb-2 sm:me-1">
-                <Label
-                  htmlFor="fname"
-                  value="First Name"
-                  className="after:ml-0.5 after:text-red-500 after:content-['*']"
-                />
+        {(e) => handleSubmit(e)}
+        {/* name */}
+        <div className="flex flex-col justify-between sm:flex-row">
+          {/* first name */}
+          <div className="mb-4 w-full sm:mb-0 sm:me-1">
+            <Label
+              htmlFor="fname"
+              value="First Name"
+              className="after:ml-0.5 after:text-red-500 after:content-['*']"
+            />
 
-                <TextInput
-                  id="fname"
-                  name="fname"
-                  placeholder="John"
-                  required
-                  type="text"
-                  className="inputs"
-                />
-                {/* error text */}
-                {errorCode === 0 && errorContainer(errorCode)}
-              </div>
-              {/* last name */}
-              <div className="sm:mb-2 sm:ms-1 w-full">
-                <Label
-                  htmlFor="lname"
-                  value="Last Name"
-                  className="after:ml-0.5 after:text-red-500 after:content-['*']"
-                />
-
-                <TextInput
-                  id="lname"
-                  name="lname"
-                  placeholder="Tyler"
-                  required
-                  type="text"
-                  className="inputs"
-                />
-                {/* error text */}
-                {errorCode === 1 && errorContainer(errorCode)}
-              </div>
-            </div>
-            {/* phone */}
-            <div>
-              <Label
-                htmlFor="phone"
-                value="Phone No"
-                className="after:ml-0.5 after:text-red-500 after:content-['*']"
-              />
-
-              <TextInput
-                id="phone"
-                name="phone"
-                placeholder="+94xxxxxxxxx"
-                required
-                type="text"
-                className="inputs"
-              />
-              {/* error text */}
-              {[2, 6].includes(errorCode) && errorContainer(errorCode)}
-            </div>
-            {/* email */}
-            <div>
-              <Label
-                htmlFor="email"
-                value="Email"
-                className="after:ml-0.5 after:text-red-500 after:content-['*']"
-              />
-
-              <TextInput
-                id="email"
-                name="email"
-                placeholder="example@domain.com"
-                required
-                type="email"
-                className="inputs"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              {/* error text */}
-              {[3, 7, 10].includes(errorCode) && errorContainer(errorCode)}
-            </div>
-
-            {/* exsistingpassword */}
-            <div className="relative">
-              <Label
-                htmlFor="password"
-                value="Current Password"
-                className="after:ml-0.5 after:text-red-500 after:content-['*']"
-              />
-
-              <TextInput
-                id="password"
-                name="password"
-                required
-                type={isPassword ? "password" : "text"}
-                placeholder="********"
-                className="inputs"
-              />
-              <PasswordSwitcher
-                isPassword={isPassword}
-                setIsPassword={setIsPassword}
-              />
-              {/* error text */}
-              {[4, 8, 9].includes(errorCode) && errorContainer(errorCode)}
-            </div>
-            {/* New password */}
-            <div className="relative">
-              <Label
-                htmlFor="password"
-                value="New Password"
-                className="after:ml-0.5 after:text-red-500 after:content-['*']"
-              />
-
-              <TextInput
-                id="password"
-                name="password"
-                required
-                type={isPassword ? "password" : "text"}
-                placeholder="********"
-                className="inputs"
-              />
-              <PasswordSwitcher
-                isPassword={isPassword}
-                setIsPassword={setIsPassword}
-              />
-              {/* error text */}
-              {[4, 8, 9].includes(errorCode) && errorContainer(errorCode)}
-            </div>
-
-            {/* confirm new password */}
-            <div className="relative">
-              <Label
-                htmlFor="confirmPassword"
-                value="Confirm new Password"
-                className="after:ml-0.5 after:text-red-500 after:content-['*']"
-              />
-
-              <TextInput
-                id="confirmPassword"
-                name="confirmPassword"
-                required
-                type={isConfPassword ? "password" : "text"}
-                placeholder="********"
-                className="inputs"
-              />
-              <PasswordSwitcher
-                isPassword={isConfPassword}
-                setIsPassword={setIsConfPassword}
-              />
-              {/* error text */}
-              {[5, 9].includes(errorCode) && errorContainer(errorCode)}
-            </div>
-
-            {/* actions */}
-            <div className="flex w-full flex-col space-y-4">
-              {/* submit */}
-              <Button
-                type="submit"
-                name="join"
-                value="Submit"
-                className="mt-5 w-2/3 place-self-center rounded-md bg-green-400 duration-300 ease-in dark:bg-emerald-500"
-              >
-                Update
-              </Button>
-            </div>
+            <TextInput
+              id="fname"
+              name="fname"
+              placeholder="John"
+              required
+              type="text"
+              className="inputs"
+            />
+            {/* error text */}
+            {errorCode === 0 && errorContainer(errorCode)}
           </div>
+          {/* last name */}
+          <div className="w-full sm:mb-0 sm:ms-1">
+            <Label
+              htmlFor="lname"
+              value="Last Name"
+              className="after:ml-0.5 after:text-red-500 after:content-['*']"
+            />
+
+            <TextInput
+              id="lname"
+              name="lname"
+              placeholder="Tyler"
+              required
+              type="text"
+              className="inputs"
+            />
+            {/* error text */}
+            {errorCode === 1 && errorContainer(errorCode)}
+          </div>
+        </div>
+        {/* phone */}
+        <div>
+          <Label
+            htmlFor="phone"
+            value="Phone No"
+            className="after:ml-0.5 after:text-red-500 after:content-['*']"
+          />
+
+          <TextInput
+            id="phone"
+            name="phone"
+            placeholder="+94xxxxxxxxx"
+            required
+            type="text"
+            className="inputs"
+          />
+          {/* error text */}
+          {[2, 6].includes(errorCode) && errorContainer(errorCode)}
+        </div>
+        {/* email */}
+        <div>
+          <Label
+            htmlFor="email"
+            value="Email"
+            className="after:ml-0.5 after:text-red-500 after:content-['*']"
+          />
+
+          <TextInput
+            id="email"
+            name="email"
+            placeholder="example@domain.com"
+            required
+            type="email"
+            className="inputs"
+            // onChange={(e) => setEmail(e.target.value)}
+          />
+          {/* error text */}
+          {[3, 7, 10].includes(errorCode) && errorContainer(errorCode)}
+        </div>
+        {/* exsistingpassword */}
+        <div className="relative">
+          <Label
+            htmlFor="password"
+            value="Current Password"
+            className="after:ml-0.5 after:text-red-500 after:content-['*']"
+          />
+
+          <TextInput
+            id="password"
+            name="password"
+            required
+            type={isPassword ? "password" : "text"}
+            placeholder="********"
+            className="inputs"
+          />
+          <PasswordSwitcher
+            isPassword={isPassword}
+            setIsPassword={setIsPassword}
+          />
+          {/* error text */}
+          {[4, 8, 9].includes(errorCode) && errorContainer(errorCode)}
+        </div>
+        {/* New password */}
+        <div className="relative">
+          <Label
+            htmlFor="password"
+            value="New Password"
+            className="after:ml-0.5 after:text-red-500 after:content-['*']"
+          />
+
+          <TextInput
+            id="password"
+            name="password"
+            required
+            type={isPassword ? "password" : "text"}
+            placeholder="********"
+            className="inputs"
+          />
+          <PasswordSwitcher
+            isPassword={isPassword}
+            setIsPassword={setIsPassword}
+          />
+          {/* error text */}
+          {[4, 8, 9].includes(errorCode) && errorContainer(errorCode)}
+        </div>
+        {/* confirm new password */}
+        <div className="relative">
+          <Label
+            htmlFor="confirmPassword"
+            value="Confirm new Password"
+            className="after:ml-0.5 after:text-red-500 after:content-['*']"
+          />
+
+          <TextInput
+            id="confirmPassword"
+            name="confirmPassword"
+            required
+            type={isConfPassword ? "password" : "text"}
+            placeholder="********"
+            className="inputs"
+          />
+          <PasswordSwitcher
+            isPassword={isConfPassword}
+            setIsPassword={setIsConfPassword}
+          />
+          {/* error text */}
+          {[5, 9].includes(errorCode) && errorContainer(errorCode)}
+        </div>
+        {/* actions */}
+        <div className="flex w-full flex-col space-y-4">
+          {/* submit */}
+          <Button
+            type="submit"
+            name="join"
+            value="Submit"
+            className="mt-5 w-2/3 place-self-center rounded-md bg-green-400 duration-300 ease-in dark:bg-emerald-500"
+          >
+            Update
+          </Button>
         </div>
       </form>
     </Card>
--- src/Components/Customer/ViewOldPayments.jsx
+++ src/Components/Customer/ViewOldPayments.jsx
@@ -1,27 +1,45 @@
-import React, { lazy } from "react";
-const ViewPayments = lazy(() => import("./ViewPayments"));
+import React from "react";
 
 const ViewOldPayments = () => {
   return (
-    <div className="h-auto w-full p-4 font-Poppins">
-      <div className="pb-5 text-center text-xl font-medium md:text-2xl">
-        View Old Payments
+    <React.Fragment>
+      <div className="pb-5 text-center  text-2xl font-medium md:text-2xl">
+        My Payments
       </div>
-      <div className="rounded-md bg-white px-2 py-4 shadow-md drop-shadow-lg dark:bg-slate-700">
-        <div className="hidden justify-between md:flex">
-          <span className="px-4">Type</span>
-          <span className="">Amount</span>
-          <span className="px-4">DateTime</span>
-          <span className="px-4">Option</span>
+      <div className="hidden w-full rounded-t-md bg-gray-400 px-4 py-2 ring-[0.5px] ring-gray-400 dark:bg-gray-700 dark:ring-gray-600 md:flex">
+        <div className="w-full text-center text-xl">
+          <span className="">Payment Type</span>
         </div>
-        <ViewPayments
-          Payment
-          Type={"Online"}
-          Amount={"2000"}
-          DateTime={"2023"}
-        />
+        <div className="w-full text-center text-xl">
+          <span className="">Amount </span>
+        </div>
+        <div className="w-full text-center text-xl">
+          <span className="">Option</span>
+        </div>
       </div>
-    </div>
+      {Array.from(Array(10).keys()).map((i) => {
+        return (
+          <div
+            key={i}
+            className="text-md group flex w-full flex-col justify-center space-y-2 rounded-sm bg-white ring-[0.5px] ring-gray-400 hover:bg-gray-200 dark:bg-slate-950 dark:ring-gray-600 dark:hover:bg-gray-800 md:flex-row md:items-center md:justify-between md:space-y-0"
+          >
+            <p className="flex w-full truncate bg-slate-100  px-4 py-2.5  group-hover:bg-gray-200 dark:bg-slate-900 group-hover:dark:bg-gray-800">
+              <span className="block md:hidden">Payment Type :&ensp;</span>
+             Onlime
+            </p>
+            <p className="flex w-full truncate px-4 py-2 ">
+              <span className="block md:hidden">Amount :&ensp;</span>
+              200
+            </p>
+            <div className="flex w-full justify-end bg-slate-100 px-4 py-2 group-hover:bg-gray-200 dark:bg-slate-900 group-hover:dark:bg-gray-800 md:justify-center">
+              <button className="rounded-md bg-sky-500 px-8 py-0.5 font-medium text-white duration-300 ease-in hover:bg-sky-700">
+                View
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </React.Fragment>
   );
 };
 export default ViewOldPayments;
