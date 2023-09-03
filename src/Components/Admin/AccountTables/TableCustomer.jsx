df3ddbc480f6a0be051523cca5f4ee4ea586006c
Kavindu Buddhika <cst20064@std.uwu.ac.lk>
2023-09-03 17:48:01 +0530

    accounts loading

--- src/Components/Admin/AccountTables/TableCustomer.jsx
+++ src/Components/Admin/AccountTables/TableCustomer.jsx
@@ -1,12 +1,57 @@
-import React, { lazy } from "react";
+import React, { lazy, useCallback, useEffect, useState } from "react";
+import axios from "axios";
 const Paginations = lazy(() => import("../Paginations"));
 
+const formData = new FormData();
+formData.append("account_type", "customer");
+
 const TableCustomer = () => {
+  const [tableData, setTableData] = useState(null);
+  const [pagesCount, setPagesCount] = useState(0);
+  const [status, setStatus] = useState("verified");
+
+  formData.append("status", status);
+
+  // session management function
+  const fetch = useCallback(() => {
+    axios
+      .post("/load_accounts", formData)
+      .then((response) => {
+        if (response.data.length !== 0) {
+          setTableData(response.data);
+          setPagesCount(Math.ceil(response.data.length / 10));
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  // session function run in component mount
+  useEffect(() => {
+    fetch();
+  }, [fetch,status]);
+
   return (
     <React.Fragment>
       <h1 className="w-full py-4 text-center font-Poppins text-xl font-medium md:text-2xl">
         Customer Accounts
       </h1>
+      <div className="relative flex justify-end py-6">
+        <button
+          onClick={() => {
+            setStatus(status === "verified" ? "deactivated" : "verified");
+          }}
+          className="w-fit rounded-sm bg-sky-600 px-4 py-1 text-sm font-medium text-white duration-300 ease-in hover:bg-sky-700 dark:bg-cyan-600 dark:hover:bg-cyan-800"
+        >
+          {status === "verified" ? (
+            <span>Deactivated</span>
+          ) : (
+            <span>Active</span>
+          )}
+          &ensp;Accounts
+        </button>
+      </div>
       <div className="hidden rounded-t-md bg-gray-400 px-4 py-2 ring-[0.5px] ring-gray-400 dark:bg-gray-700 dark:ring-gray-600 md:flex">
         <div className="w-full text-center">
           <span className="">First Name</span>
@@ -24,43 +69,43 @@ const TableCustomer = () => {
           <span className="">Option</span>
         </div>
       </div>
-      {Array.from(Array(10).keys()).map((i) => {
-        return (
-          <div
-            key={i}
-            className="text-md group flex flex-col justify-center space-y-2 rounded-sm bg-white ring-[0.5px] ring-gray-400 hover:bg-gray-200 dark:bg-slate-950 dark:ring-gray-600 dark:hover:bg-gray-800 md:flex-row md:items-center md:justify-between md:space-y-0"
-          >
-            <p className="flex w-full truncate bg-slate-100 px-4 py-2 group-hover:bg-gray-200 dark:bg-slate-900 group-hover:dark:bg-gray-800">
-              <span className="block md:hidden">First Name :&ensp;</span>
-              Kavindu
-            </p>
-            <p className="flex w-full truncate px-4 py-2">
-              <span className="block md:hidden">Last Name :&ensp;</span>
-              Buddhika
-            </p>
-            <p className="flex w-full truncate bg-slate-100 px-4 py-2 group-hover:bg-gray-200 dark:bg-slate-900 group-hover:dark:bg-gray-800">
-              <span className="block md:hidden">Tel :&ensp;</span>
-              0704512641
-            </p>
-            <p className="flex w-full truncate px-4 py-2">
-              <span className="block md:hidden">Email :&ensp;</span>
-              kavindulakshan2980@gmail.com
-            </p>
-            <div className="flex w-full justify-end truncate bg-slate-100 px-4 py-2 group-hover:bg-gray-200 dark:bg-slate-900 group-hover:dark:bg-gray-800 md:justify-center">
-              <button className=" px-8 font-medium text-sky-500 duration-300 ease-in hover:text-sky-700">
-                View
-              </button>
+      {tableData === null && (
+        <p className="mt-4 w-full text-center text-sm font-medium italic">
+          No Data Found
+        </p>
+      )}
+      {tableData !== null &&
+        tableData.map((data, i) => {
+          return (
+            <div
+              key={i}
+              className="text-md group flex flex-col justify-center space-y-2 rounded-sm bg-white ring-[0.5px] ring-gray-400 hover:bg-gray-200 dark:bg-slate-950 dark:ring-gray-600 dark:hover:bg-gray-800 md:flex-row md:items-center md:justify-between md:space-y-0"
+            >
+              <p className="flex w-full truncate bg-slate-100 px-4 py-2 group-hover:bg-gray-200 dark:bg-slate-900 group-hover:dark:bg-gray-800">
+                <span className="block md:hidden">First Name :&ensp;</span>
+                {data.Customer_firstname}
+              </p>
+              <p className="flex w-full truncate px-4 py-2">
+                <span className="block md:hidden">Last Name :&ensp;</span>
+                {data.Customer_lastname}
+              </p>
+              <p className="flex w-full truncate bg-slate-100 px-4 py-2 group-hover:bg-gray-200 dark:bg-slate-900 group-hover:dark:bg-gray-800">
+                <span className="block md:hidden">Tel :&ensp;</span>
+                {data.Customer_Tel}
+              </p>
+              <p className="flex w-full truncate px-4 py-2">
+                <span className="block md:hidden">Email :&ensp;</span>
+                {data.Email}
+              </p>
+              <div className="flex w-full justify-end truncate bg-slate-100 px-4 py-2 group-hover:bg-gray-200 dark:bg-slate-900 group-hover:dark:bg-gray-800 md:justify-center">
+                <button className=" px-8 font-medium text-sky-500 duration-300 ease-in hover:text-sky-700">
+                  View
+                </button>
+              </div>
             </div>
-          </div>
-        );
-      })}
-      <Paginations />
-
-      <div className="relative flex justify-center py-6">
-        <button className="w-fit rounded-md bg-sky-600 px-8 py-1 font-medium text-white duration-300 ease-in hover:bg-sky-700 dark:bg-cyan-600 dark:hover:bg-cyan-800">
-          View Deactivated Customer Accounts
-        </button>
-      </div>
+          );
+        })}
+      <Paginations totpages={pagesCount} />
     </React.Fragment>
   );
 };
